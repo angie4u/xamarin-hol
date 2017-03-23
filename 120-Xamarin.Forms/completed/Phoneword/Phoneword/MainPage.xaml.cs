5aca4d6630dfb5dd736538db5c8963a396f8ed9f
Eunji Kim (DX) <eunk@microsoft.com>
2017-03-23 17:28:27 +0900

    add src code

---
+++ 120-Xamarin.Forms/completed/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + phoneNumberText.Text + "?",
+                "Yes",
+                "No"))
+            {
+
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    await dialer.DialAsync(phoneNumberText.Text);
+                }
+
+            }
+
+        }
+
+        public interface IDialer
+        {
+            Task<bool> DialAsync(string number);
+        }
+        
+    }
+}
