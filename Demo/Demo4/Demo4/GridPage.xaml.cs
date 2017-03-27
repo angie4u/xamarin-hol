91d809b4bc6481ba84e49f92467bfa8c692dfe22
Eunji Kim (DX) <eunk@microsoft.com>
2017-03-28 01:31:11 +0900

    add demo4

---
+++ Demo/Demo4/Demo4/GridPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridPage : ContentPage
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
